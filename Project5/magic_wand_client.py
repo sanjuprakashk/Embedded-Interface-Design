534472033880d5b64ea143fbdff10807238ea416
Sanju Prakash Kannioth <Sanju.Kannioth@colorado.edu>
2019-12-10 22:33:27 -0700

    Add client integration code without lex

---
+++ Project5/magic_wand_client.py
@@ -0,0 +1,79 @@
+#!/usr/bin/python3
+
+import picamera
+import boto3
+import json
+import random
+import os
+
+''' 
+https://www.geeksforgeeks.org/random-numbers-in-python/
+https://stackoverflow.com/questions/27276135/python-random-system-time-seed
+'''
+import time
+random.seed(time.time())
+
+camera = picamera.PiCamera()
+camera.vflip = True
+camera.capture('test_img.jpg')
+
+
+client = boto3.client("rekognition",
+                      # aws_access_key_id =access_key_id,
+                      # aws_secret_access_key = secret_access_key,
+                      region_name='us-east-1')
+photo = "test_img.jpg"
+
+with open(photo, "rb") as source_image:
+    source_bytes = source_image.read()
+
+response = client.detect_labels(Image={'Bytes': source_bytes}, 
+                                MaxLabels=1)
+
+print(response['Labels'])
+
+polly_client = boto3.Session(
+    #             aws_access_key_id=,                     
+    # aws_secret_access_key=,
+    region_name='us-east-1').client('polly')
+
+response_polly = polly_client.synthesize_speech(VoiceId='Joanna',
+                OutputFormat='mp3', 
+                Text = 'Wand says '+response['Labels'][0]['Name'])
+
+file = open('speech.mp3', 'wb')
+file.write(response_polly['AudioStream'].read())
+file.close()
+
+import pygame
+pygame.mixer.init()
+pygame.mixer.music.load("speech.mp3")
+pygame.mixer.music.play()
+while pygame.mixer.music.get_busy() == True:
+    continue
+
+sqs = boto3.client('sqs', region_name='us-east-1')
+
+queueUrl = "https://sqs.us-east-1.amazonaws.com/837538385441/magic_wand.fifo"
+
+label = response['Labels'][0]['Name']
+sendToSqs = json.dumps([{"voiceCommand": "Wrongo"}, {"Label" : str(response['Labels'][0]['Name'])}])
+
+response_sqs = sqs.send_message(
+    QueueUrl=queueUrl,
+    MessageBody=sendToSqs,
+    MessageGroupId='wand',
+    MessageDeduplicationId=str(random.random())
+    
+)
+
+os.rename('test_img.jpg', label+".jpg")
+bucket_name = "test-bucket-eid-123"
+object_name = label+".jpg"
+file_name = object_name
+
+s3_client = boto3.client('s3')
+try:
+    response = s3_client.upload_file(file_name, bucket_name, object_name)
+except:
+    print('s3 upload error')
--- Project5/magic_wand_server.py
+++ Project5/magic_wand_server.py
@@ -26,7 +26,9 @@ class magic_wand_server(QtWidgets.QDialog):
 		
 		self.counter = 1;
 		
-		self.total_messages = 0
+		self.latestLabel = ' '
+		
+		self.total_messages = 1
 		self.totalImages = 1
 		
 		self.ui.getImageButton.clicked.connect(self.getImage)
@@ -39,56 +41,71 @@ class magic_wand_server(QtWidgets.QDialog):
 		
 		sqs_queue_url = "https://sqs.us-east-1.amazonaws.com/837538385441/magic_wand.fifo"
 		sqs_client = boto3.client('sqs', region_name='us-east-1')
-		num_msgs=3
+		num_msgs=1
 		wait_time=0
 		visibility_time=5
-
-		#try:
-		msgs = sqs_client.receive_message(QueueUrl=sqs_queue_url,
-									  MaxNumberOfMessages=num_msgs,
-									  WaitTimeSeconds=wait_time,
-									  VisibilityTimeout=visibility_time)
-	
-		index = 0
-		msgs = msgs['Messages']
-		voice_command_correct = self.numVoiceCorrect
-		image_command_correct = self.numImagesCorrect
-		try:
-			for i in msgs:
-				self.total_messages = self.total_messages + 1
-				incoming_message = json.loads(msgs[index]['Body'])
-				print(incoming_message)
-				voice_command = incoming_message[0]['voiceCommand']
-				index = index + 1
-				if voice_command in valid_voice_commands:
-					voice_command_correct = voice_command_correct + 1
-					if(voice_command == 'Correcto'):
-						image_command_correct = image_command_correct + 1
-						self.totalImages = self.totalImages + 1
-						print("Image correct")
-					elif(voice_command == 'Wrongo'):
-						if(image_command_correct >= 1):
-							image_command_correct = image_command_correct - 1
-						self.totalImages = self.totalImages + 1
-						print("Image wrong")
-
-				else:
-					pass
-				print(voice_command)
-		except:
-			print("erorr")
 		
+		doneFlag = 0
+	
+		voice_command_correct = 0
+		image_command_correct = 0
+		
+		while(doneFlag == 0):
+			try:
+				msgs = sqs_client.receive_message(QueueUrl=sqs_queue_url,
+											  MaxNumberOfMessages=num_msgs,
+											  WaitTimeSeconds=wait_time,
+											  VisibilityTimeout=visibility_time)
+			
+				index = 0
+				msgs = msgs['Messages']
+				voice_command_correct = self.numVoiceCorrect
+				image_command_correct = self.numImagesCorrect
+				for i in msgs:
+					self.total_messages = self.total_messages + 1
+					incoming_message = json.loads(msgs[index]['Body'])
+					sqs_client.delete_message(QueueUrl=sqs_queue_url, ReceiptHandle=msgs[index]['ReceiptHandle'])
+					#print(incoming_message)
+					voice_command = incoming_message[0]['voiceCommand']
+					index = index + 1
+					if voice_command in valid_voice_commands:
+						voice_command_correct = voice_command_correct + 1
+						if(voice_command == 'Correcto'):
+							self.latestLabel = incoming_message[1]['Label']
+							self.totalImages = self.totalImages + 1
+							image_command_correct = image_command_correct + 1
+							#print("Image correct. Label = "+ str(self.latestLabel))
+						elif(voice_command == 'Wrongo'):
+							self.latestLabel = incoming_message[1]['Label']
+							self.totalImages = self.totalImages + 1
+							#print("Image wrong. Label = "+ str(self.latestLabel))
+					
+						else:
+							pass
+						print(voice_command)
+					self.numImagesCorrect = image_command_correct
+					self.numVoiceCorrect = voice_command_correct
+				
+			except:
+				doneFlag = 1
+				print("erorr")
+			
 		self.percentVoiceCorrect = (float(voice_command_correct / self.total_messages) * 100)
 		self.percentVoiceWrong = 100 - self.percentVoiceCorrect
 		self.ui.percentCorrectVoiceDisplay.setText(str(self.percentVoiceCorrect) + "%")
 		self.ui.percentWrongVoiceDisplay.setText(str(self.percentVoiceWrong) + "%")
-		self.numVoiceCorrect = voice_command_correct
+		
 		
 		self.percentImagesCorrect = (float(image_command_correct / self.totalImages) * 100)
 		self.percentImagesWrong = 100 - self.percentImagesCorrect
 		self.ui.percentCorrectImagesDisplay.setText(str(self.percentImagesCorrect) + "%")
 		self.ui.percentWrongImagesDisplay.setText(str(self.percentImagesWrong) + "%")
-		self.numImagesCorrect = image_command_correct
+		
+		
+		self.ui.imageLabel.setText(str(self.latestLabel))
+		
+		doneFlag = 0
+		
 
 	def getPercentImages(self):
 		pass
@@ -96,9 +113,10 @@ class magic_wand_server(QtWidgets.QDialog):
 	def getImage(self):
 		s3 = boto3.resource('s3', region_name='us-east-1')
 		bucket_name = "test-bucket-eid-123"
-		object_name = "2.jpg"
+		object_name = self.latestLabel+".jpg"
+		print(object_name)
 		s3.Bucket(bucket_name).download_file(object_name, object_name)
-		self.ui.imageView.setPixmap(QtGui.QPixmap("2.jpg"))
+		self.ui.imageView.setPixmap(QtGui.QPixmap(object_name))
 
 def main():
 	'''
