822c88ad94a1faab3e493d12e1da322bee4d6073
srinath-95 <srsh9018@colorado.edu>
2019-12-10 23:19:14 -0700

    microhone with button

--- Project5/microphone.py
+++ Project5/microphone.py
@@ -1,51 +1,41 @@
 #!/usr/bin/python3 
-
 import pyaudio
 import wave
 import os
+import boto3
+import RPi.GPIO as GPIO # Import Raspberry Pi GPIO library
+from time import sleep
+import copy
 
-#The following code comes from markjay4k as referenced below
-form_1 = pyaudio.paInt16
-chans=1
-samp_rate = 44100
-chunk = 4096
-record_secs = 5    #record time
-dev_index = 2
-wav_output_filename = 'test1.wav'
+client = boto3.client('lex-runtime', region_name="us-east-1")
 
+def button_callback(channel):
+    print("Button was pushed!")
+    #setup audio input stream
+    sleep (1)
+    os.system('arecord -D plughw:1,0 -d 3 -r 16000 -f S16_LE -t wav test1.wav &&  aplay test1.wav')
 
-audio = pyaudio.PyAudio()
-
-#setup audio input stream
-stream=audio.open(format = form_1,rate=samp_rate,channels=chans, input_device_index = dev_index, input=True, frames_per_buffer=chunk)
-print("recording")
-frames=[]
-
-for ii in range(0,int((samp_rate/chunk)*record_secs)):
-    data=stream.read(chunk,exception_on_overflow = False)
-    frames.append(data)
-
-print("finished recording")
-
-stream.stop_stream()
-stream.close()
-audio.terminate()
-
-#creates wave file with audio read in
-#Code is from the wave file audio tutorial as referenced below
-wavefile=wave.open(wav_output_filename,'wb')
-wavefile.setnchannels(chans)
-wavefile.setsampwidth(audio.get_sample_size(form_1))
-wavefile.setframerate(samp_rate)
-wavefile.writeframes(b''.join(frames))
-wavefile.close()
-
-import pygame
-pygame.mixer.init()
-pygame.mixer.music.load("test1.wav")
-pygame.mixer.music.play()
-while pygame.mixer.music.get_busy() == True:
-    continue
+    #CONVERTING SPEECH TO TEXT USING AMAZON LEX
+    obj = wave.open('/home/pi/Desktop/Embedded-Interface-Design/Project5/test1.wav','rb')
+    response_lex = client.post_content(
+        botName='MagicWand',
+        botAlias='MagicWand',
+        userId='test',
+        contentType='audio/l16; rate=16000; channels=1',
+        accept='text/plain; charset=utf-8',
+        inputStream=obj.readframes(96044)
+    )
     
-#plays the audio file
-#os.system("aplay test1.wav")
+    response = response_lex['message']
+    if response == "Sorry, can you please repeat that?":
+        response = 'Invalid'
+        
+    print(response)
+    
+GPIO.setwarnings(False) # Ignore warning for now
+GPIO.setmode(GPIO.BOARD) # Use physical pin numbering
+GPIO.setup(10, GPIO.IN, pull_up_down=GPIO.PUD_DOWN) # Set pin 10 to be an input pin and set initial value to be pulled low (off)
+GPIO.add_event_detect(10,GPIO.RISING,callback=button_callback) # Setup event on pin 10 rising edge
+message = input("Press enter to quit\n\n") # Run until someone presses enter
+
+GPIO.cleanup() # Clean up
