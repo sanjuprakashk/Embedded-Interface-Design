f7006c036de2b34db73b0c9b3c23431be5e76aa3
srinath-95 <srsh9018@colorado.edu>
2019-12-11 00:43:07 -0700

    nodejs

---
+++ Project5/web_socket_server.js
@@ -0,0 +1,83 @@
+// Node.js WebSocket server script
+const http = require('http');
+const WebSocketServer = require('websocket').server;
+const server = http.createServer();
+server.listen(9898);
+const wsServer = new WebSocketServer({
+    httpServer: server
+});
+var mysql = require('mysql');
+
+var con = mysql.createConnection({
+  host: "localhost",
+  user: "srisan",
+  password: "srisan1234",
+  database: "super_project"
+});
+
+con.connect(function(err) {
+  if (err) throw err;
+  console.log("Connected!");
+});
+
+wsServer.on('request', function(request) {
+    const connection = request.accept(null, request.origin);
+    connection.on('message', function(message) {
+    if(message.utf8Data == "getData") {
+      console.log('Received Message:', message.utf8Data);
+      var ret = [];
+      con.query("SELECT * FROM MAGICWAND1", function (err, result, fields) {
+      if (err) throw err;
+        ret = JSON.stringify(result);
+        console.log(ret);
+        connection.sendUTF(ret);
+    
+      });
+    }
+
+    /*
+    if(message.utf8Data == "getReadings") {
+      //console.log('Received Message:', message.utf8Data);
+      
+      con.query("SELECT Humd FROM HUMID ORDER BY id DESC LIMIT 1", function (err, result_hum, fields) {
+      if (err) throw err;
+        ret_hum = result_hum[0].Humd
+        console.log('ret_hum:', result_hum[0].Humd);
+
+      });
+      
+      con.query("SELECT Temp FROM TEMP ORDER BY id DESC LIMIT 1", function (err, result_temp, fields) {
+      if (err) throw err;
+        ret_temp = result_temp[0].Temp
+        console.log('ret_temp:', result_temp[0].Temp);
+        
+        readings = {temp:ret_temp, hum:ret_hum};
+        console.log('ret:', readings);
+        
+        connection.sendUTF(JSON.stringify(readings));
+
+      });
+       
+       
+    }
+
+    if(message.utf8Data == "GetLast10HumVal") {
+      //console.log('Received Message:', message.utf8Data);
+      var ret = [];
+      con.query("SELECT Humd FROM HUMID ORDER BY id DESC LIMIT 3", function (err, result, fields) {
+      if (err) throw err;
+        ret = JSON.stringify(result);
+        //console.log(ret);
+        connection.sendUTF(ret);
+      });
+    }*/
+    
+    else {
+      console.log('Received Message: NULL');
+    }
+      connection.sendUTF('Hi this is WebSocket server!');
+    });
+    connection.on('close', function(reasonCode, description) {
+        console.log('Client has disconnected.');
+    });
+});
